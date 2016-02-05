bc0b9e6867fd9cf450eae51ace733037be7f4cb6
Saeid <Alidadi>
2016-02-05 22:10:05 +0330

    connected to mongodb and checks for images existance

--- static.js
+++ static.js
@@ -1,76 +1,132 @@
 //this is for static files that dosent need any value insertion befor sending to client
 var fs = require('fs');
+var path = require('path');
 var thumbnail = require('jimp');
+var mongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+
+
+//inserting image to database
+var insertImage = function(db, name, format, big, thumbnail) {
+
+	var image = {
+		"name" : name,
+		"big" : big,
+		"thumbnail" : thumbnail,
+		"format" : format
+	}
+
+	db.collection('images').insertOne(image);
+}
+
+//checking existance of image in database
+var existsIndb = function(name, callback) {
+
+	mongoClient.connect('mongodb://localhost:27017/images', function(err, db) {
+		if(err) {
+			throw "An error in database connection";
+		}
+		else {
+			var result = db.collection("images").find({"name" : name });
+			//console.log('Ok, connected to Mongodb client' + result);
+			result.each(function(err, doc) {
+				if(doc == null ) {
+					console.log('The image not exist in database.');
+					callback(false);
+				}
+				else {
+					callback(true);
+				}
+			});
+		}
+		db.close();
+	});
+}
+
+//answer to request after loading files
+function answer(data, type, response) {
+
+	response.writeHead(200,{'Content-Type': type });
+	response.write(data);
+	response.end();
+}
 
 function home(response){
 
 	var dir = __dirname + '\\index.html';
 	var data = fs.readFileSync(dir,'utf8');
-	response.writeHead(200,{'Content-Type':'text/html'});
-	response.write(data);
-	response.end();
-
+	answer(data, 'text/html', response);
 }
 
-function style(request, response){
+function style(request, response) {
 
 	var dir = __dirname + '\\style\\' + request.url.replace('/','');
 	fs.readFile(dir, function(err, data){
-		response.writeHead(200,{'Content-Type':'text/css'});
-		response.write(data);
-		response.end();
+		answer(data, 'text/css', response);
 	});	
 
 }
 
 function script(request, response) {
 	var dir = __dirname + '\\script\\' + request.url.replace('/','');
-	fs.readFile(dir, function(err, data){
-		response.writeHead(200,{'Content-Type':'text/javascript'});
-		response.write(data);
-		response.end();
+	fs.readFile(dir, function(err, data) {
+		answer(data, 'text/javascript', response);
 	});
 }
 
 function image(request, response) {
 
-	var thumb_dir = __dirname + '\\images\\thumbnails\\' + request.url.replace('/','');
-	var big_dir = __dirname + '\\images\\' + request.url.replace('/','');
-	fs.exists(thumb_dir, function(exist) {
-		//checking for existance of thumbnail image in thumbnails folder
-		if(exist) {console.log(thumb_dir);
-			fs.readFile(thumb_dir, function(err, data) {
-				response.writeHead(200,{'Content-Type':'image/jpg'});
-				response.write(data);
-				response.end();
-			});
-		} 
-		else if(!exist) {
-			fs.exists(big_dir, function(exist){
-				if(exist) {
-					thumbnail.read(big_dir, function (err, lenna) {
-   						if (err) throw err;
-    					lenna.resize(256, 256)   // resize 
-  			      	 	.quality(30)             // set JPEG quality 
-   			      	 	.greyscale()             // set greyscale
-   			      	 	.write(thumb_dir)        
-    		     	 	.getBuffer(thumbnail.MIME_JPEG, function(err, buffer) {
-    		    			response.writeHead(200,{'Content-Type':'image/jpg'});
-							response.write(buffer);
-							response.end();
-					 	});
-					});
+	var name = request.url.replace('/','');
+	var thumb_dir = __dirname + '\\images\\thumbnails\\';
+	var thumb_path = thumb_dir + name;
+	var big_path = __dirname + '\\images\\' + name;
+
+	//checks to find that the image thumbnail and big exist in database
+	existsIndb(name, function(existindb) {
+		if(!existindb) {
+			//if the thumbnails directory not exsits then creat it
+			fs.mkdir(thumb_dir, function(err) {
+				if(err && err.code == 'EEXIST') {
+					console.log('directory exist');
 				}
+			});
+
+			//checking for existance of thumbnail image in thumbnails folder
+			fs.exists(thumb_path, function(exist) {
+				if(exist) {
+					fs.readFile(thumb_path, function(err, data) {
+						answer(data, 'image/jpg', response);
+					});
+				} 
+				else if(!exist) {
+					fs.exists(big_path, function(exist) {
+						if(exist) {
+							thumbnail.read(big_path, function (err, lenna) {
+		   						if (err) throw err;
+		    					lenna.resize(256, 256)   // resize 
+		  			      	 	.quality(30)             // set JPEG quality 
+		   			      	 	.greyscale()             // set greyscale
+		   			      	 	.write(thumb_path)        
+		    		     	 	.getBuffer(thumbnail.MIME_JPEG, function(err, buffer) {
+		    		    			answer(buffer, 'image/jpg', response);
+							 	});
+							});
+						}
+						else {
+							response.end();
+						}
+					});		
+				} 
 				else {
+					console.log('thes is no more images! whit the path of: ' + dir);
 					response.end();
 				}
-			});		
-		}
+			});	
+		} 
 		else {
-			console.log('thes is no more images! whit the path of: ' + dir);
-			response.end();
+
 		}
-	});	
+	});
 }
 
 module.exports.home = home;
